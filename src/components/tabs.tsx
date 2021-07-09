e8c5b03e707a3d7b4ab4530654145f2678a8b856
YogPanjarale <yog.panjarale@gmail.com>
2021-07-10 00:03:02 +0530

    made profile and logout

--- src/components/tabs.tsx
+++ src/components/tabs.tsx
@@ -1,23 +1,27 @@
 import * as React from 'react';
 import { BottomNavigation, Text } from 'react-native-paper';
 import HomeScreen from '../screens/HomeScreen';
+import ProfileScreen from '../screens/ProfileScreen';
 import ScheduleScreen from '../screens/ScheduleScreen';
 
 const HomeRoute = () => <HomeScreen/>;
 
 const ScheduleRoute = () => <ScheduleScreen/>
 
+const AccountRoute = ()=><ProfileScreen/>
 
 export default () => {
   const [index, setIndex] = React.useState(0);
   const [routes] = React.useState([
     { key: 'home', title: 'Home', icon: 'home' },
-    { key: 'schedule', title: 'schecule', icon: 'calendar-clock' },
+    { key: 'schedule', title: 'Schecule', icon: 'calendar-clock' },
+    { key: 'account', title: 'Account', icon: 'account' },
   ]);
 
   const renderScene = BottomNavigation.SceneMap({
     home:HomeRoute ,
     schedule: ScheduleRoute,
+    account: AccountRoute,
   });
 
   return (
---
+++ src/screens/ProfileScreen.tsx
@@ -0,0 +1,45 @@
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+import { auth } from '../firebase'
+import * as firebase from 'firebase'
+import { Button } from 'react-native-paper'
+type State = {
+ user:firebase.default.User|null
+}
+
+export default class ProfileScreen extends Component<{},State> {
+    constructor(props: {} | Readonly<{}>) {
+      super(props);
+    }
+    componentDidMount = () => {
+        const user  = auth().currentUser
+        this.setState({user})
+    };
+    logout = () => {
+        auth().signOut()
+
+    }
+    render() {
+        return (
+            <View style={styles.container}>
+                {/* <Text> Profile Screen </Text> */}
+                {/* <Text>{JSON.stringify(this.state?.user)}</Text> */}
+                <Text style={styles.text}>signed in as {this.state?.user?.email}</Text>
+                <Button  mode="contained" onPress={()=>{this.logout()}}>Log out</Button>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+		flex: 1,
+		justifyContent: "center",
+		alignItems: "center",
+	},
+    text:{
+        fontSize:20,
+        margin:10,
+        fontWeight:'500'
+    }
+})
